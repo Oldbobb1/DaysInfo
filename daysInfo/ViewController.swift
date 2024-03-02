733362f016f240ad2e9dead5587300b1019f32fe
Bobbi R <bobbi@Noutbuk-Bobbi.local>
2024-03-02 12:34:20 +0300

    element initialization

--- daysInfo/ViewController.swift
+++ daysInfo/ViewController.swift
@@ -7,16 +7,60 @@
 import UIKit
 import SnapKit
 
-
 class ViewController: UIViewController {
-
+    
+    private lazy var numberOfDays = ""
+    
+    private let firstLabel: UILabel =  {
+        let label = UILabel()
+        label.text = "Радуйся каждому дню!"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 25)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let secondLabel: UILabel =  {
+        let label = UILabel()
+        label.text = "Выбери cвой день рождения:"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let thirdLabel: UILabel =  {
+        let label = UILabel()
+        label.text = "Ты наслаждаешься жизнью дней"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let datePicker: UIDatePicker = {
+        let date = UIDatePicker()
+        date.datePickerMode = .date
+        date.calendar = .current
+        date.preferredDatePickerStyle = .wheels
+        return date
+    }()
+    
+    private let buttonCreat: UIButton = {
+        let button = UIButton()
+        button.setTitle("Result", for: .normal)
+        button.layer.cornerRadius = 11
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-
+        
     }
-
-
+    
+    
 }
 
